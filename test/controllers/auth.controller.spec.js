c4374b0f6b663d12b6da2deaf2d63ed144d5011a
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-08-04 19:41:45 -0400

    add setRoles function; no arrow functions

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -5,14 +5,27 @@ let authController = require('../../controllers/auth.controller');
 
 console.log(authController);
 
-describe('AuthController', ()=>{
-    context('isAuthorized', ()=>{
-        it('should return false if not authorized', ()=> {
-            assert.equal(false, authController.isAuthorized(['user'], 'admin'));
+describe('AuthController', function(){
+    context('isAuthorized', function(){
+        it('should return false if not authorized', function(){
+            authController.setRoles(['user']);
+            assert.equal(false, authController.isAuthorized('admin'));
         });
 
-        it('should return true if authorized', ()=> {
-            assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'));
+        it('should return true if authorized', function() {
+            authController.setRoles(['user', 'admin']);
+            assert.equal(true, authController.isAuthorized('admin'));
+        });
+    });
+
+    context('isAuthorizedAsync', function() {
+        it('should return false if not authorized', function(done) {
+            authController.setRoles(['user', 'admin']);
+            authController.isAuthorizedAsync(['user', 'admin']
+                                             , function(isAuth) {
+                                                 assert.equal(false, isAuth);
+                                                 done();
+                                             });
         });
     });
 });
